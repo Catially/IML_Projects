9db11b9ed21e31d0d337c230391edf19b3bd295c
Jin Cheng <chengjin222@outlook.com>
2021-04-17 21:35:20 +0200

    HistGradientBoosting

--- task2/main.py
+++ task2/main.py
@@ -1,25 +1,51 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
-from collections import Counter
-import xgboost as xgb
+from sklearn.experimental import enable_hist_gradient_boosting
+from sklearn.ensemble import HistGradientBoostingClassifier, HistGradientBoostingRegressor
 from sklearn import linear_model
 
 
-def features_engineering(df_ori):
-    df_ori = df_ori.drop(columns=['Time'])
-    feature = df_ori.groupby(['pid'], sort=False).agg([np.max, np.min, np.mean, np.median, np.std]) # feature augmentation
-    feature = feature.fillna(feature.mean()) # fill nan with mean
+def features_engineering(df):
+    features = [np.nanmax, np.nanmin, np.nanmean, np.nanmedian, np.nanstd]
+    df = df.drop(columns=['Time'])
+    data = df.groupby(['pid'], sort=False).agg(features) # feature augmentation
+    data = data.fillna(data.median()) # fill nan with median
+    # transformer = StandardScaler()
     transformer = RobustScaler()
-    X = transformer.fit_transform(feature)
+    X = transformer.fit_transform(data)
     return X
+
+# def features_engineering(data):
+#     data = data.to_numpy()
+#     n = 12
+#     x = []
+#     features = [np.nanmedian, np.nanmean, np.nanvar, np.nanmin,np.nanmax]
+
+#     for index in range(int(data.shape[0] / n)):
+#         assert data[n * index, 0] == data[n * (index + 1) - 1, 0], \
+#         'Ids are {}, {}'.format(data[n * index, 0], data[n * (index + 1) - 1, 0])
+#         patient_data = data[n * index: n * (index + 1), 2:]
+#         feature_values = np.empty((len(features), data[:, 2:].shape[1]))
+#         for i, feature in enumerate(features):
+#             feature_values[i] = feature(patient_data, axis=0)
+#         x.append(feature_values.ravel())
+
+#     x = np.array(x)
+#     imp = SimpleImputer(strategy='median')
+#     x = imp.fit_transform(x)
+#     tf = StandardScaler()
+#     x = tf.fit_transform(x)
+#     return x
 
 # read data
 df_train = pd.read_csv('./datasets/train_features.csv')
-X_train = features_engineering(df_train)
 df_label = pd.read_csv('./datasets/train_labels.csv')
+X_train = features_engineering(df_train)
 labels = list(df_label.columns)
 labels.remove('pid')
 
@@ -32,63 +58,51 @@ print('Preprocessed!\n')
 # print(X_train)
 # print(X_test)
 
-# parameters found by grid search
-# uncomment commented lines to run grid search
-# learning rate, max_depth, subsample
-param = np.zeros((11, 3))
-param[0] = [0.1, 5.0, 0.9] # LABEL_BaseExcess
-param[1] = [0.1, 3.0, 0.75] # LABEL_Fibrinogen
-param[2] = [0.15, 3.0, 0.9] # LABEL_AST
-param[3] = [0.15, 3.0, 1.0] # LABEL_Alkalinephos
-param[4] = [0.1, 3.0, 0.7] # LABEL_Bilirubin_total
-param[5] = [0.1, 3.0, 0.75] # LABEL_Lactate
-param[6] = [0.1, 3.0, 0.5] # LABEL_TroponinI
-param[7] = [0.05, 5.0, 0.75] # LABEL_SaO2
-param[8] = [0.05, 3.0, 0.75] # LABEL_Bilirubin_direct
-param[9] = [0.05, 5.0, 0.75] # LABEL_EtCO2
-param[10] = [0.05, 3.0, 1.0] # LABEL_Sepsis
+# l2_regularization, learning_rate, max_depth
+params = np.zeros((15, 3))
+params[0] = [0.001, 0.05, 7]
+params[1] = [0.1, 0.1, 4]
+params[2] = [0.001, 0.05, 8]
+params[3] = [0.01, 0.05, 9]
+params[4] = [0.0001, 0.05, 4]
+params[5] = [0.0, 0.1, 8]
+params[6] = [0.001, 0.05, 3]
+params[7] = [0.01, 0.05, 7]
+params[8] = [0.01, 0.05, 4]
+params[9] = [0.0, 0.05, 4]
+params[10] = [0.0, 0.05, 3]
+params[11] = [0.01, 0.05, 5]
+params[12] = [0.0, 0.05, 9]
+params[13] = [0.01, 0.15, 6]
+params[14] = [0.0001, 0.05, 3]
 
-# # Task 1 & 2
+
+# Task 1 & 2
 for i in range(11):
     label = labels[i]
     y_train = df_label[label]
 
-    # # imbalanced rate
-    # neg, pos = np.bincount(y_train)
-    # total = neg + pos
-    # print(label, ': Positive: {} ({:.2f}% of total)'.format(pos, 100 * pos / total))
-
     # # grid search
     # parameters = {
-    #     'max_depth':[3, 4, 5, 6, 7, 8, 9], 
     #     'learning_rate':[0.05, 0.10, 0.15, 0.20],
-    #     'subsample':[0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    #     'max_depth':[3, 4, 5, 6, 7, 8, 9], 
+    #     'l2_regularization':[0.0, 0.0001, 0.001, 0.01, 0.1]
     # }
-    # count = Counter(y_train)
-    # imbalance = count[0] / count[1]
-    # xgbclf = xgb.XGBClassifier(scale_pos_weight=imbalance, use_label_encoder=False, eval_metric='logloss')
-    # clf = GridSearchCV(estimator=xgbclf, param_grid=parameters, cv=5, scoring='roc_auc', verbose=4, n_jobs=-1)
-    # clf.fit(X_train, y_train)
-    # print(clf.cv_results_)
-    # results = pd.DataFrame(clf.cv_results_)
-    # name = label + '.csv'
-    # results.to_csv(name)
 
-    count = Counter(y_train)
-    imbalance = count[0] / count[1]
-    xgbclf = xgb.XGBClassifier(
-        learning_rate=param[i][0],
-        max_depth=int(param[i][1]), 
-        subsample=param[i][2],
-        scale_pos_weight=imbalance, 
-        use_label_encoder=False, 
-        eval_metric='logloss'
-        )
+    # model = HistGradientBoostingClassifier(max_iter=300)
 
-    # cv_score = cross_val_score(xgbclf, X_train, y_train, cv=5, scoring='roc_auc')
-    # print('CV_score: ', np.mean(cv_score))
+    # GDSCV = GridSearchCV(estimator=model, param_grid=parameters, cv=5, scoring='roc_auc', n_jobs=-1)
+    # GDSCV.fit(X_train, y_train)
+    # print(GDSCV.best_params_)
 
-    model = xgbclf.fit(X_train, y_train)
+    # cv_score = cross_val_score(model, X_train, y_train, cv=5, scoring='roc_auc', n_jobs=-1)
+    # print("Cross-validation score is {score:.3f},"
+    #     " standard deviation is {err:.3f}"
+    #     .format(score = cv_score.mean(), err = cv_score.std()))
+
+    model = HistGradientBoostingClassifier(max_iter=300, l2_regularization=params[i][0], learning_rate=params[i][1], max_depth=int(params[i][2]))
+
+    model = model.fit(X_train, y_train)
     prob = model.predict_proba(X_test)
     prob = np.array(prob[:, 1])
     y_pred[label] = prob
@@ -101,27 +115,26 @@ for i in range(11, len(labels)):
     label = labels[i]
     y_train = df_label[label]
 
-    # grid search
+    # # grid search
     # parameters = {
     #     'learning_rate':[0.05, 0.10, 0.15, 0.20],
     #     'max_depth':[3, 4, 5, 6, 7, 8, 9], 
-    #     'subsample':[0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    #     'l2_regularization':[0.0, 0.0001, 0.001, 0.01, 0.1]
     # }
 
-    xgbreg = xgb.XGBRegressor(use_label_encoder=False)
-    # model = GridSearchCV(estimator=xgbreg, param_grid=parameters, cv=5, scoring='r2', verbose=4, n_jobs=-1)
-    # model.fit(X_train, y_train)
-    # print(model.cv_results_)
-    # results = pd.DataFrame(model.cv_results_)
-    # name = label + '.csv'
-    # results.to_csv(name)
+    # model = HistGradientBoostingClassifier(max_iter=300)
 
-    mdl = linear_model.Lasso(alpha=0.1)
+    # GDSCV = GridSearchCV(estimator=model, param_grid=parameters, cv=5, scoring='r2', n_jobs=-1)
+    # GDSCV.fit(X_train, y_train)
+    # print(GDSCV.best_params_)
 
-#     cv_score = cross_val_score(model, X_train, y_train, cv=5, scoring='r2')
-#     print('CV_score: ', np.mean(cv_score))
+    # cv_score = cross_val_score(model, X_train, y_train, cv=5, scoring='r2', n_jobs=-1)
+    # print("Cross-validation score is {score:.3f},"
+    #     " standard deviation is {err:.3f}"
+    #     .format(score = cv_score.mean(), err = cv_score.std()))
 
-    model = mdl
+    model = HistGradientBoostingRegressor(max_iter=300, l2_regularization=params[i][0], learning_rate=params[i][1], max_depth=int(params[i][2]))
+
     model = model.fit(X_train, y_train)
     y_pred_label = model.predict(X_test)
     y_pred[label] = y_pred_label
